2fcc2204150b1555858ea530f8fb0df6e68085c0
Medhmo <chrisconde18@gmail.com>
2022-04-25 18:50:35 +0000

    Adds the trackers for guesses and letter and also the initial codes for the main loop

--- run.py
+++ run.py
@@ -6,6 +6,7 @@ import random
 # Computer will generate a random word from a list
 # Player tries to guess the full word
 # Or one letter at the time
+
 # User welcome and username selection
 
 USERNAME = str(input("Please enter a Username : "))
@@ -74,4 +75,26 @@ MAX_WRONG = len(HANGMANPICS) - 1
 
 word = random.choice(WORDS)
 
-print(word)
+# In words letters masking
+
+current_guess = "-" * len(word)
+
+# Wrong guesses tracker
+
+wrong_guesses = 0
+
+# Used letters tracker
+
+used_letters = []
+
+# Main loop creation
+
+print("Welcome to Hangman")
+print("Computer will generate a random word from a list")
+print("Try guessing the word")
+
+while wrong_guesses < MAX_WRONG and current_guess != word :
+
+    #print(HANGMANPICS[wrong_guesses])
+
+
