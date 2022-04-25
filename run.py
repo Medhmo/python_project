18bca5d9fcd9365f8a76d3195d59cf6e383e3a98
Medhmo <chrisconde18@gmail.com>
2022-04-25 18:17:04 +0000

    Adds the Hangmanpics constants

--- run.py
+++ run.py
@@ -1,8 +1,65 @@
+""" Imports"""
+
+import random
 
 # Hangman Game
 # Computer will generate a random word from a list
 # Player tries to guess the full word
 # Or one letter at the time
+# User welcome and username selection
 
-USERNAME = str(input("Please enter your name"))
-print("Welcome" + USERNAME + "Best of luck ! ...")
+USERNAME = str(input("Please enter a Username : "))
+print("Welcome " + USERNAME + " Best of luck ! ")
+
+# Constants
+
+HANGMANPICS = ['''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========''']
